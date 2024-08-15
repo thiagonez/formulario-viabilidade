cc3af6a56d90f89f18fda25d71794e791864479a
Thiago <thiagorxsplanilhas@gmail.com>
2024-08-15 16:49:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    // Rolagem suave para as seções ao clicar nos links do menu
     $('nav ul li a').click(function(e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 1000);
+            scrollTop: $($(this).attr('href')).offset().top - 50
+        }, 1000); // Tempo de rolagem de 1 segundo
     });
 
-    // Example of additional interactivity
-    $('.btn-secondary').hover(function() {
-        $(this).css('background-color', '#f47c3c');
-    }, function() {
-        $(this).css('background-color', '#0056b3');
+    // Alterar o fundo do menu ao rolar a página
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('#navbar').css('background', 'rgba(0, 115, 230, 0.8)');
+        } else {
+            $('#navbar').css('background', 'rgba(0, 115, 230, 0.9)');
+        }
     });
 });
