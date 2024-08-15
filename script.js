e72a09a57a08effb0dae771ed82a2514444cd5d8
Thiago <thiagorxsplanilhas@gmail.com>
2024-08-15 16:38:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-$(document).ready(function () {
-    // Rolagem suave para as seções ao clicar nos links do menu
-    $('nav a').on('click', function (e) {
-        if (this.hash !== "") {
-            e.preventDefault();
-            const hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800);
-        }
+$(document).ready(function(){
+    $('nav ul li a').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+    });
+
+    // Example of additional interactivity
+    $('.btn-secondary').hover(function() {
+        $(this).css('background-color', '#f47c3c');
+    }, function() {
+        $(this).css('background-color', '#0056b3');
     });
 });
